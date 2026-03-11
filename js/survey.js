d626bdde6d563829d6faf69b596cc01c0d3a1622
k-ayls <kayleeasmith04@hotmail.com>
2026-03-10 23:47:32 -0300

    feat: profile page creation

--- js/survey.js
+++ js/survey.js
@@ -52,16 +52,3 @@ form.addEventListener('submit', function (event) {
     window.location.href = "planning.php"; // redirect to planning page
 });
 
-//display form info
-const displayButton = document.getElementById("display-button");
-displayButton.addEventListener("click", displayForm); //on click, display form data
-
-function displayForm(){
-const surveyObject = JSON.parse(localStorage.getItem("survey"));
-    //loop over object.entries
-    let surveyData = "";
-        for (const [key, value] of Object.entries(surveyObject)){
-            surveyData += key + ": " + value + "<br>";
-        }
-        document.getElementById("display-area").innerHTML = surveyData; //show form data in <p>
-    }
