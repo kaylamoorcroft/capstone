888549511fd917c6cdc99f829c16f67ff103de0d
k-ayls <kayleeasmith04@hotmail.com>
2026-03-22 02:04:33 -0300

    feat: course information page

--- course-info.php
+++ course-info.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
+<?php
+    include "dbConnection.php";
+    $sql_statement = "SELECT id, title, `description`, subjectCode, `number` FROM courses WHERE MinimumCredits != 0 AND subjectCode = 'COMP'";
+    $result = mysqli_query($con, $sql_statement);
+?>
 <html lang="en">
 <head>
-    <link rel="icon" href="images/logo.png" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <script src="https://kit.fontawesome.com/ededc88243.js" crossorigin="anonymous"></script> -->
+    <script src="https://kit.fontawesome.com/ededc88243.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&icon_names=account_box,delete,edit_square,info" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
@@ -13,8 +17,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     <title>Course Map</title>
 </head>
-<body style="background-color: rgb(220, 239, 248)">
-    <nav class="pc-navbar">
+<body>
+<!-- <a href="planner-collection.html" class="btn btn-primary">Back</a> -->
+<nav class="pc-navbar">
         <div class="pc-menu">
             <!-- <i class="fa fa-bars pc-bars" aria-hidden="true"></i> -->
             <span class="material-icons pc-bars" style="font-size: 30px;">dehaze</span>
@@ -95,12 +100,42 @@
                 navBar.classList.remove("open");
         });
     </script>
-    <div id="reset-planner" class="p-3 w-100"><span type="button" class="material-icons material-symbols-outlined float-end">delete</span></div>
-    <p class="empty-msg d-none">The planner is empty. Choose "plan a semester" from the sidebar to start planning.</p>
-    <div class="grid" id="plan-collection">
+
+    <div class="container" style="width: 65%">
+        <input id="course_search" type="text" class="form-control" placeholder="Search for courses..." style="margin: 40px; margin-left: auto;">
     
+        <div class="accordion" id="course-accordion">
+                    <?php
+                        while($row = mysqli_fetch_array($result)) {
+                            echo "<div class='accordion-item'>
+                                    <h2 class='accordion-header'>
+                                        <button class='accordion-button collapsed' type='button' data-bs-toggle='collapse' data-bs-target='#panels".$row['id']."' aria-expanded='false' aria-controls='panels".$row['id']."'>
+                                            ".$row['title']."
+                                        </button>
+                                    </h2>
+                                    <div id='panels".$row['id']."' class='accordion-collapse collapse' data-bs-parent='#course-accordion'>
+                                        <div class='accordion-body row'>
+                                            <h5 style='padding-left: 0'>Description</h5>
+                                                ".$row['description']."
+                                                <br><br>
+                                            <h5 style='padding-left: 0'>Requirements</h5>
+                                                ...
+                                                <br><br>
+                                            <h5 style='padding-left: 0'>Course ID</h5>
+                                                ".$row['subjectCode']."-".$row['number']."
+                                        </div>
+                                    </div>
+                                </div>";
+                        }
+                        // Free result set
+                        mysqli_free_result($result);
+                        mysqli_close($con);
+                    ?>
+        </div>
+
     </div>
-    
-    <script src="js/plan-collection.js"></script>
+
 </body>
 </html>
+
+        
