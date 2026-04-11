ff8ba9cf1eb07971b6752377a7c1c5252e3d668c
kaylamoorcroft <kayla.moorcroft@icloud.com>
2026-04-11 14:30:26 -0300

    feat: populate survey with saved responses

--- public/js/pages/survey.js
+++ public/js/pages/survey.js
@@ -1,33 +1,28 @@
-$("#comp-year").on("change", () => {
+function compYearDisplayToggle() {
     //show first year preference options
     if ($("#comp-year").val() == "1") {
         $("#first-year-group").removeClass("d-none");
         $('#first-year-pref').prop('required', true);
-    }
-
-    else {
+    } else {
         $("#first-year-group").addClass("d-none");
         $("#first-year-pref").val("");
         $('#first-year-pref').prop('required', false);
     }
-});
+}
 
-$("#degree-level").on("change", () => {
+function degreeLevelDisplayToggle() {
     //show postgraduate degree options
     if ($("#degree-level").val() == "postgrad") {
         $("#postgrad-group").removeClass("d-none");
         $('#postgrad-option').prop('required', true);
-    }
-
-    else {
+    } else {
         $("#postgrad-group").addClass("d-none");
         $("#postgrad-option").val("");
         $('#postgrad-option').prop('required', false);
     }
-});
+}
 
-// listener for comp type value change
-$("#comp-type").on("change", () => {
+function compTypeDisplayToggle() {
     // show applied option dropdown if comp type is applied
     if ($("#comp-type").val() == "BACS") {
         $("#applied-option-group").removeClass("d-none");
@@ -39,7 +34,27 @@ $("#comp-type").on("change", () => {
         $("#applied-option").val("");
         $('#applied-option').prop('required', false); // applied option field is now not required
     }
-});
+}
+
+// change listeners
+$("#comp-year").on("change", compYearDisplayToggle);
+$("#degree-level").on("change", degreeLevelDisplayToggle);
+$("#comp-type").on("change", compTypeDisplayToggle);
+
+
+// load in previous responses if exist
+const prefs = JSON.parse(localStorage.getItem("survey")) || {};
+if (prefs != {}) {
+    console.log('Here are youre previous responses for the survey:');
+    for (const field in prefs) {
+        console.log(`${field}: ${prefs[field]}`);
+        $(`[name="${field}"]`).val(prefs[field]);
+        // show extra fields depending on the prefs
+        if (field == 'comp-year') compYearDisplayToggle();
+        else if (field == 'degree-level') degreeLevelDisplayToggle();
+        else if (field == 'comp-type') compTypeDisplayToggle();
+    }
+}
 
 //store form info in localStorage
 const form = document.getElementById('survey');
